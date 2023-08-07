c0828b7c894d1610be151f7945af452ec5548a0f
snakesgun <snakesgun@gmail.com>
2023-08-07 16:14:38 +0800

    update user config

---
+++ options.lua
@@ -0,0 +1,10 @@
+return {
+    opt = {
+        guifont = "CaskaydiaCove Nerd Font:h10"
+    },
+    g = {
+        neovide_cursor_vfx_mode = "railgun",
+        neovide_floating_blur_amount_x = 2.0,
+        neovide_floating_blur_amount_y = 2.0
+    }
+}
