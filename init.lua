dc20d79aae3aa333ce313002c922b856966633e3
snakesgun <snakesgun@gmail.com>
2023-08-08 12:17:36 +0800

    add cursor reset

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("ExitPre", {
     group = vim.api.nvim_create_augroup("Exit", { clear = true }),
-    command = "set guicursor=a:hor80",
+    command = "set guicursor=a:hor20-blinkwait700-blinkoff400-blinkon250",
     desc = "Set cursor back to beam when leaving Neovim."
 })
 
